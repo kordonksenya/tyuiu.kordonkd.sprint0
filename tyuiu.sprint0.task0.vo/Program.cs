e70cbd2c245078876d135b9904a1ceb594b01340
user <user@WIN-OA952C5G3QD>
2024-09-21 14:02:06 +0500

    Add project files.

---
+++ tyuiu.sprint0.task0.vo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.sprint0.task0.vo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
