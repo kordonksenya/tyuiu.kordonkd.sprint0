c6cb934728c00438d0de1603b0da2b85241730a9
kordonksenya <cuteaudcixpie@gmail.com>
2024-09-21 20:10:46 +0500

    task1

---
+++ tyuiu.kordonkd.sprint0.task1.v0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.kordonkd.sprint0.task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня зовут "+name+" мне "+age+" лет!");
+            Console.ReadKey();
+
+        }
+        
+    }
+}
