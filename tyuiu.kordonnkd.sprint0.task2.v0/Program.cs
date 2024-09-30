67b5d6ad6311bcdcdb4d9b2bcd3ba4a7e0c61c78
kordonksenya <cuteaudcixpie@gmail.com>
2024-09-30 11:47:58 +0500

    task2

--- tyuiu.kordonnkd.sprint0.task2.v0/Program.cs
+++ tyuiu.kordonnkd.sprint0.task2.v0/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- using tyuiu.kordonkd.sprint0.task2.v0.lib
+using tyuiu.kordonkd.sprint0.task2.v0.lib;
 
 namespace tyuiu.kordonnkd.sprint0.task2.v0
 {
