587ad8cc7d1977d0fd6f0bf41db138a99ad90955
kordonksenya <cuteaudcixpie@gmail.com>
2024-10-16 12:22:07 +0500

    task7

---
+++ tyuiu.kordonkd.sprint0.task7.v0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tyuiu.kordonkd.sprint0.task7.v0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tyuiu.kordonkd.sprint0.task7.v0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e20e2c7e-5019-46eb-9067-980cd8cdf0bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
